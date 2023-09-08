cb986eeb651e3947818f478d55e440b48814ff81
Rafael <rsuzuki>
2023-09-08 16:44:10 -0300

    feat:: logger creation

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"github.com/rafasuzuki/goportunities.git/config"
 	"github.com/rafasuzuki/goportunities.git/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil{
+		logger.Errorf("config initialization erro: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
